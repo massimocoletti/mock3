0b408ee6fd5518183b792952d59d1a971bdc6849
massimocoletti <massimo.coletti@gmail.com>
2019-10-02 08:50:40 +0200

    inserita card lavorazioni

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { OrderheaderComponent }  
        from './orderheader/orderheader.component';
+import { LavorazioniComponent }
+       from './lavorazioni/lavorazioni.component';
+import { LavorazioniActComponent }  
+       from './lavorazioni-act/lavorazioni-act.component';
 
 
 import { AppComponent } from './app.component';
 import { CustomMaterialModule } from './material.module';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, FlexLayoutModule, CustomMaterialModule ],
-  declarations: [ AppComponent , OrderheaderComponent],
+  declarations: [ AppComponent , OrderheaderComponent
+                  , LavorazioniComponent, LavorazioniActComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/lavorazioni/lavorazioni.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lavorazioni',
+  templateUrl: './lavorazioni.component.html',
+  styleUrls: ['./lavorazioni.component.css']
+})
+export class LavorazioniComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
