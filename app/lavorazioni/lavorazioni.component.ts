f170cdb7171c4be9697a5d32d4c8cb2fa8a78782
massimocoletti <massimo.coletti@gmail.com>
2019-10-02 09:25:38 +0200

    ok con un iteratore

--- app/lavorazioni/lavorazioni.component.ts
+++ app/lavorazioni/lavorazioni.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { lavlist } from '../lavorazioni.data';
+
 @Component({
   selector: 'app-lavorazioni',
   templateUrl: './lavorazioni.component.html',
   styleUrls: ['./lavorazioni.component.css']
 })
 export class LavorazioniComponent implements OnInit {
+    lavorazioni =  [
+{
+  codice:'000000010',
+  nome:'Fresatura'
+},
+{
+  codice:'000000020',
+  nome:'Verniciatura'
+}
+];
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit() {
   }
