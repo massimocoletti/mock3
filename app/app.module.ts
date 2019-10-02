f79c06492ccaf6ae01b85b013662d1254721e4f0
massimocoletti <massimo.coletti@gmail.com>
2019-10-02 08:25:05 +0200

    aggiunto OrderheaderComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { OrderheaderComponent }  
+       from './orderheader/orderheader.component';
 
 
 import { AppComponent } from './app.component';
 import { CustomMaterialModule } from './material.module';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, FlexLayoutModule, CustomMaterialModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent , OrderheaderComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
